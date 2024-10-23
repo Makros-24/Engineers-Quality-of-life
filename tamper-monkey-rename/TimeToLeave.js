4af23115834122f51e0a6007584d2bc5f5649987
Ben Yarou Nidhal <57046325+nidyran@users.noreply.github.com>
2024-10-23 17:05:02 +0100

    feat: display the humanized remaining duration !
    Yaay

--- tamper-monkey-rename/TimeToLeave.js
+++ tamper-monkey-rename/TimeToLeave.js
@@ -1,58 +1,62 @@
 // ==UserScript==
-// @name         TTL V2
+// @name         TTL V3
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  I want to go home!
-// @author       You
-// @match        https://*/*/
+// @author       Not Me
+// @match        https://*/**
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-// Define your work hours
-const workHoursPerDay = 8; //TODO this must be dynamic
 
-// Get the current worked time from the website
-const workedTimeString = document.querySelector("#kt_content > div > div:nth-child(5) > div.col-xl-9.col-lg-12.order-lg-1.order-xl-1 > div:nth-child(3) > div > div.kt-ribbon__target > i > span").innerText;
-debugger;
-const workedTimeArray = workedTimeString.split(':');
-const hoursWorked = parseInt(workedTimeArray[1]);
-const minutesWorked = parseInt(workedTimeArray[2]);
-const secondsWorked = parseInt(workedTimeArray[3]);
+    var tag = document.createElement("script");
+    tag.src = "https://cdn.jsdelivr.net/npm/date-fns@4.1.0/cdn.min.js";
+    tag.defer = "true";
+    document.getElementsByTagName("body")[0].appendChild(tag);
 
-// Convert worked time to minutes
-const totalMinutesWorked = hoursWorked * 60 + minutesWorked + secondsWorked / 60;
+    // Ensure the script is loaded before running the rest of the code
+    tag.onload = function() {
+        // Define your work hours
+        const workHoursPerDay = 8;
 
-// Calculate the remaining time to work
-const remainingWorkMinutes = workHoursPerDay * 60 - totalMinutesWorked;
+        // Get the current worked time from the website
+        const workedTimeString = document.querySelector("#kt_content > div > div:nth-child(5) > div.col-xl-9.col-lg-12.order-lg-1.order-xl-1 > div:nth-child(3) > div > div.kt-ribbon__target > i > span").innerText;
 
-// Calculate the time to leave
-const now = new Date();
-const leaveTime = new Date(now.getTime() + remainingWorkMinutes * 60 * 1000);
+        const workedTimeArray = workedTimeString.split(':');
+        const hoursWorked = parseInt(workedTimeArray[1]);
+        const minutesWorked = parseInt(workedTimeArray[2]);
+        const secondsWorked = parseInt(workedTimeArray[3]);
 
-// Format the leave time
-const leaveHours = leaveTime.getHours();
-const leaveMinutes = leaveTime.getMinutes();
-const leaveSeconds = leaveTime.getSeconds();
+        // Convert worked time to minutes
+        const totalMinutesWorked = hoursWorked * 60 + minutesWorked + secondsWorked / 60;
 
-// Create and style the leave time box
-const leaveTimeBox = document.createElement('div');
-leaveTimeBox.id = 'leave-time-box';
-leaveTimeBox.style.backgroundColor = '#f0f0f0';
-leaveTimeBox.style.border = '1px solid #ccc';
-leaveTimeBox.style.borderRadius = '5px';
-leaveTimeBox.style.padding = '10px';
-leaveTimeBox.style.marginTop = '20px';
+        // Calculate the remaining time to work
+        const remainingWorkMinutes = workHoursPerDay * 60 - totalMinutesWorked;
 
-if (leaveTime > now.getTime()) {
-// Append the leave time box to the body of the page
-document.querySelector("#kt_content > div > div:nth-child(5) > div.col-xl-9.col-lg-12.order-lg-1.order-xl-1 > div:nth-child(3) > div > h5").innerText=`YOU MUST LEAVE AT ${leaveHours}:${leaveMinutes}:${leaveSeconds} `;
-} else {
-// Append the leave time box to the body of the page
-document.querySelector("#kt_content > div > div:nth-child(5) > div.col-xl-9.col-lg-12.order-lg-1.order-xl-1 > div:nth-child(3) > div > h5").innerText=` WHAT ARE YOU DOING HERE `;
-}
+        // Calculate the time to leave
+        const now = new Date();
+        const leaveTime = new Date(now.getTime() + remainingWorkMinutes * 60 * 1000);
 
+        // Format the leave time
+        const leaveHours = leaveTime.getHours();
+        const leaveMinutes = leaveTime.getMinutes();
+        const leaveSeconds = leaveTime.getSeconds();
 
+        // Humanize the remaining time using date-fns
+        const remainingDuration = dateFns.intervalToDuration({
+            start: 0,
+            end: remainingWorkMinutes * 60 * 1000
+        });
+        const humanizedDuration = dateFns.formatDuration(remainingDuration);
+
+        // Update the page with the leave time and remaining duration
+        if (leaveTime > now) {
+            document.querySelector("#kt_content > div > div:nth-child(5) > div.col-xl-9.col-lg-12.order-lg-1.order-xl-1 > div:nth-child(3) > div > h5").innerText = `YOU MUST LEAVE AT ${leaveHours}:${leaveMinutes}:${leaveSeconds}, REMAINING ${humanizedDuration}`;
+        } else {
+            document.querySelector("#kt_content > div > div:nth-child(5) > div.col-xl-9.col-lg-12.order-lg-1.order-xl-1 > div:nth-child(3) > div > h5").innerText = ` WHAT ARE YOU DOING HERE `;
+        }
+    };
 })();
